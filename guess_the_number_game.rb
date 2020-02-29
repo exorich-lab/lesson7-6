c3e3340fbaa389806d5a9621d1cdb549cbe3f7cc
sergejapetenok <exorich@yandex.ru>
2020-02-29 14:12:39 +0400

    Optimisation

--- guess_the_number_game.rb
+++ guess_the_number_game.rb
@@ -1,29 +1,22 @@
 puts "Загадано число от 0 до 15, отгадайте какое с 3 попыток?"
 lucky_number = rand (15)
-count = 1
-until count > 3 do
+count = 0
+until count == 3 do
+  count += 1
   choice = gets.to_i
   warm_diff = (lucky_number - choice).abs
-  if lucky_number == choice
+  if warm_diff <= 2
+    temp = " Горячо!"
+  else warm_diff >= 3
+    temp = " Холодно!"
+  end
+  if lucky_number > choice && count != 3
+    puts "Загаданное число больше." + temp.to_s
+  elsif lucky_number < choice && count != 3
+  puts "Загаданное число меньше." + temp.to_s
+  elsif lucky_number == choice
     puts "Ура, вы выиграли c " + count.to_s + " попытки!"
-  elsif lucky_number > choice && count < 3
-    puts "Загаданное число больше"
-    if warm_diff <= 2
-      puts "Горячо"
-    else
-      warm_diff >= 3
-      puts "Холодно"
-    end
-  elsif lucky_number < choice && count < 3
-    puts "(Загаданное число меньше)"
-    if warm_diff <= 2
-      puts "Горячо"
-    else
-      warm_diff >= 3
-      puts "Холодно"
-    end
-  else lucky_number != choice && count == 3
+  else
     puts "Вы проиграли! " + "Было загаданно число: " + lucky_number.to_s
   end
-  count += 1
 end
